6f409466468b83d878443bb80c78a2c6c50c563d
tanishqandmac <tanishq15105@iiitd.ac.in>
2018-09-18 19:26:01 +0530

    API Keys Inserted

--- twitterAPI.py
+++ twitterAPI.py
@@ -1,2 +1,4 @@
 import twitter
-api = twitter.Api(consumer_key='consumer_key',consumer_secret='consumer_secret',access_token_key='access_token',access_token_secret='access_token_secret')
+api = twitter.Api(consumer_key='XLfgWE4ZqTpW0fToKTxR4jS1W',consumer_secret='7wMtcgUTl1WSUsWpT5L8rtJKNkeLOBjvTU9ef1FvQTNr7YyEQ3',access_token_key='2451495793-Od8i2RbABlbQA6OUQ1ljP2LbTlRJTL1uRMGvDO3',access_token_secret='Gl56ztlJXzCkgvc7AsDDlbPFVcLWIIFphDNkAFDaZOGPp')
+users = api.GetFriends()
+print([u.name for u in users])
